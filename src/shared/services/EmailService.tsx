96c5a6b9970a9c7bb1bdfcff786130afe8a9402d
kseniask <kseniask30@icloud.com>
2023-12-10 16:40:52 -0800

    clog key

--- src/shared/services/EmailService.tsx
+++ src/shared/services/EmailService.tsx
@@ -2,8 +2,8 @@ import emailjs from 'emailjs-com';
 import { RsvpFormValues } from '../../types/RsvpFormValues';
 
   export const SendEmail = async (formData: RsvpFormValues) => {
+    console.log(process.env.REACT_APP_PUBLIC_KEY)
     const templateParams = {
-      to_name: process.env.RECIPIENT_NAME,
       attending: formData.attending.toLocaleUpperCase(),
       "number-of-guests": formData.numberOfGuests,
       "guests-name": formData.guestName,
@@ -13,6 +13,6 @@ import { RsvpFormValues } from '../../types/RsvpFormValues';
       'service_y26k1lt',
       'template_9hptep5',
       templateParams,
-      `${process.env.REACT_APP_PUBLIC_KEY}`
+      process.env.REACT_APP_PUBLIC_KEY
     );
   };
