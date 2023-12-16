6da89600e0b9e03d1d65222da1b5223c857de38d
kseniask <kseniask30@icloud.com>
2023-12-16 15:09:30 -0800

    updated pictures

--- src/components/ColorPalette.tsx
+++ src/components/ColorPalette.tsx
@@ -1,25 +1,32 @@
-import React, { useContext } from "react";
-import StImage from "../shared/styled/StImage";
-import colorPalette from './../../public/images/ColorPalette.png';
-import StFlexBoxContainer from "../shared/styled/StFlexBoxContainer";
-import StHeader from "../shared/styled/StHeader";
-import GlobalDataContext from "../context/GlobalDataContext";
-import StText from "../shared/styled/StText";
-import StDiv from "../shared/styled/StDiv";
+import React, { useContext } from 'react';
+import StImage from '../shared/styled/StImage';
+import colorPalette from './../images/ColorPalette.png';
+import StFlexBoxContainer from '../shared/styled/StFlexBoxContainer';
+import StHeader from '../shared/styled/StHeader';
+import GlobalDataContext from '../context/GlobalDataContext';
+import StText from '../shared/styled/StText';
+import StDiv from '../shared/styled/StDiv';
 
 const ColorPalette = () => {
-    const {themeValues} = useContext(GlobalDataContext);
+  const { themeValues } = useContext(GlobalDataContext);
 
-    return (
+  return (
     <StFlexBoxContainer margin="50px 0">
-        <StDiv opacity="70%" textAlign="center">
-            <StHeader margin="0">{themeValues.colorPalette}</StHeader>
-            <StText fontFamily="BirthstoneBounce" fontSize="35px" margin="0">{themeValues.formal}</StText>
-            <StText fontSize="20px" letterSpacing="2px" marginBottom="40px" dangerouslySetInnerHTML={{__html: themeValues.dressCodeMessage}}/>
-         </StDiv>
-        <StImage src={colorPalette} width="70%"/>
-       
-    </StFlexBoxContainer>)
-}
+      <StDiv opacity="70%" textAlign="center">
+        <StHeader margin="0">{themeValues.colorPalette}</StHeader>
+        <StText fontFamily="BirthstoneBounce" fontSize="35px" margin="0">
+          {themeValues.formal}
+        </StText>
+        <StText
+          fontSize="20px"
+          letterSpacing="2px"
+          marginBottom="40px"
+          dangerouslySetInnerHTML={{ __html: themeValues.dressCodeMessage }}
+        />
+      </StDiv>
+      <StImage src={colorPalette} width="70%" />
+    </StFlexBoxContainer>
+  );
+};
 
 export default ColorPalette;
